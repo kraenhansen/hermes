f0f299ac27cbc36ad32b96b6b0517dfb45912890
Kræn Hansen <mail@kraenhansen.dk>
2025-01-25 20:23:39 +0100

    Delete js_runtime api

---
+++ API/node-api/hermes_node-api.h
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#ifndef HERMES_HERMES_NODE_API_H
+#define HERMES_HERMES_NODE_API_H
+
+#include "js_native_api.h"
+
+EXTERN_C_START
+
+// TODO: Declare a function to create a napi_env from a HermesRuntime
+
+EXTERN_C_END
+
+#endif // !HERMES_HERMES_NODE_API_H
